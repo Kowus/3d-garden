6c4ef29cd58ea814a71e5a015430b3170c8039b9
kowus <barnabas@elite-education.org>
2017-08-10 17:54:58 +0000

    simple DHT11 test successful

--- _3d-garden.ino
+++ _3d-garden.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+#define DHTPIN 8
+#define DHTTYPE DHT11
 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(t) || isnan(h))Serial.println("Failed to read from DHT");
+  else {
+    Serial.println("Temp = \t" + String(t) + "\tHumidity = \t" + String(h));
+    delay(500);
+  }
 }
